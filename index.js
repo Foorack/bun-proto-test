5cc5905d10da0f8656f90701c4f673f3c59aa6a4
Faxälv, Max <max.faxalv@sony.com>
2023-09-21 20:16:23 +0200

    Reduce size

--- index.js
+++ index.js
@@ -27,14 +27,9 @@ app.use(
 );
 app.use(function logging(req, res, next) {
 	// Log the request
-	const ip = req.socket.remoteAddress.indexOf(':') > -1 ? `[${req.socket.remoteAddress}]` : req.socket.remoteAddress;
-	console.log('HTTP: ' + ip + ' ' + req.method + ' ' + req.url);
+	console.log('HTTP: ' + req.method + ' ' + req.url);
 	next();
 });
-
-//
-// Configure the routes
-//
 
 // Print out proto
 app.get('/', function (req, res) {
@@ -43,18 +38,11 @@ app.get('/', function (req, res) {
 	res.status(200).end('OK : ' + req.protocol + ' : ' + runtime);
 });
 
-//
-// TLS
-//
-
-const ENV_HTTPS_KEY_PATH = './certs/cert.key';
-const ENV_HTTPS_CERT_PATH = './certs/cert.pem';
-
 // Start the application
 //http.createServer(app).listen(80, '0.0.0.0');
 const httpsOptions = {
-	key: fs.readFileSync(ENV_HTTPS_KEY_PATH),
-	cert: fs.readFileSync(ENV_HTTPS_CERT_PATH),
+	key: fs.readFileSync('./certs/cert.key'),
+	cert: fs.readFileSync('./certs/cert.pem'),
 };
 https.createServer(httpsOptions, app).listen(4343);
 
